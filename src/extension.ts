2b17c7329362f851ad0989c68001f0be070490b3
Makoto Hamanaka <ham.lua@gmail.com>
2021-10-09 17:57:24 +0900

    initial commit

---
+++ src/extension.ts
@@ -0,0 +1,94 @@
+import * as vscode from 'vscode';
+import OpenAPIClientAxios from 'openapi-client-axios';
+// cloudbuildapi.d.ts was created by typegen (openapi-client-axios-typegen) with cloudbuildapi.json
+import { Client as CloudBuildClient } from './cloudbuildapi';
+import { CloudBuildLogContentReader, CloudBuildLogContentProvider } from './logprovider'
+
+const cloudBuildLogScheme = "unitycloudbuildviewer";
+const cloudBuildLogUrlBase = "https://build-api.cloud.unity3d.com";
+
+// this method is called when your extension is activated
+// your extension is activated the very first time the command is executed
+export function activate(context: vscode.ExtensionContext) {
+	console.log("apikey: " + getApiKey());
+	const api = new OpenAPIClientAxios(
+		{
+			definition: context.extensionPath + "/cloudbuildapi.json",
+			axiosConfigDefaults: {
+				headers: {
+					'Content-Type': 'application/json',
+					'Authorization': 'Basic ' + getApiKey(),
+				},
+			},
+			withServer: { url: 'https://build-api.cloud.unity3d.com/api/v1/', description: 'cloud build api server' }
+		}
+	);
+		
+	// Use the console to output diagnostic information (console.log) and errors (console.error)
+	// This line of code will only be executed once when your extension is activated
+	console.log('Congratulations, your extension "unitycloudbuild-viewer" is now active!');
+
+	const logProvider = new CloudBuildLogContentProvider();
+	vscode.workspace.registerTextDocumentContentProvider(cloudBuildLogScheme, logProvider);
+
+	// The command has been defined in the package.json file
+	// Now provide the implementation of the command with registerCommand
+	// The commandId parameter must match the command field in package.json
+	let disposable = vscode.commands.registerCommand('unitycloudbuild-viewer.helloWorld', () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		vscode.window.showInformationMessage('Hello World from UnityCloudBuildViewer!');
+	});
+	let disposable2 = vscode.commands.registerCommand('unitycloudbuild-viewer.listBuilds', async () => {
+		// The code you place here will be executed every time your command is executed
+		// Display a message box to the user
+		const client = await api.init<CloudBuildClient>();
+		const url = await readBuilds(client);
+		let uri = vscode.Uri.parse(cloudBuildLogScheme + ":" + url);
+
+		logProvider.setApiKey(getApiKey());
+		let doc = await vscode.workspace.openTextDocument(uri); // calls back into the provider
+
+		await vscode.window.showTextDocument(doc, { preview: false });
+
+		vscode.window.showInformationMessage('List Builds from UnityCloudBuildViewer!');
+	});
+	let disposable3 = vscode.commands.registerCommand('unitycloudbuild-viewer.reload', () => {
+		if (!vscode.window.activeTextEditor) {
+			return;
+		  }
+		  let { document } = vscode.window.activeTextEditor;
+		  if (document.uri.scheme !== cloudBuildLogScheme) {
+			return;
+		  }
+		  logProvider.setApiKey(getApiKey());
+		  logProvider.reload(document.uri);
+		  //await vscode.window.showTextDocument(newUri, { preview: false });
+		  vscode.window.showInformationMessage('Reload from UnityCloudBuildViewer!');
+	});
+
+
+	context.subscriptions.push(disposable, disposable2, disposable3);
+}
+
+// this method is called when your extension is deactivated
+export function deactivate() {}
+
+function getApiKey() : string
+{
+	return vscode.workspace.getConfiguration('unitycloudbuild-viewer')?.get("apiKey") ?? "";
+}
+
+async function readProjects(client: CloudBuildClient) {
+    const res = await client.listProjectsForUser();
+    //const res = await client.getBuilds({orgid: "psychicvrlab", projectid: "styly-vr-auto-build", buildtargetid: "_all"});
+    console.log('projects', res.data);
+}
+
+async function readBuilds(client: CloudBuildClient) {
+    const res = await client.getBuilds({orgid: "psychicvrlab", projectid: "styly-vr-auto-build", buildtargetid: "_all"});
+    console.log('builds: ', res.data);
+    console.log('builds[0].links.log.href: ', res.data[0].links.log.href);
+	return cloudBuildLogUrlBase + res.data[0].links.log.href;
+}
+
---
+++ src/logprovider.ts
@@ -0,0 +1,175 @@
+import * as vscode from 'vscode';
+import axios from 'axios';
+import { IncomingMessage } from 'http';
+
+function sleep(ms) {
+	return new Promise((resolve) => {
+		setTimeout(resolve, ms);
+	});
+}
+
+export class CloudBuildLogContentReader {
+	apiKey: string;
+	uri: vscode.Uri;
+	header: string = "";
+	body: string = "";
+	footer: string = "";
+	bodyChanged: boolean = false;
+	moreDataExist: boolean = false;
+	loadEnd: boolean = false;
+	loadError: Error;
+	onDidChangeEmitter: vscode.EventEmitter<vscode.Uri>;
+	stopRequested: boolean;
+
+	public constructor(apiKey: string, uri: vscode.Uri, onDidChangeEmitter: vscode.EventEmitter<vscode.Uri>) {
+		console.log("CloudBuildLogContentReader constructor start");
+		this.apiKey = apiKey;
+		this.uri = uri;
+		this.onDidChangeEmitter = onDidChangeEmitter;
+		this.readFileStreaming(uri);
+		this.watchBodyChange(uri);
+		console.log("CloudBuildLogContentReader constructor end");
+	}
+
+	public getContent() {
+		console.log("provideTextDocumentContent called.");
+		return this.getHeaderText() + "\n" + this.body + "\n" + this.getFooterText();
+	}
+
+	stop() {
+		this.stopRequested = true;
+	}
+
+	async watchBodyChange(uri: vscode.Uri) {
+		while (!this.stopRequested && !this.loadEnd) {
+			await sleep(1000);
+			console.log("checking bodyChanged");
+			if (this.bodyChanged) {
+				this.bodyChanged = false;
+				console.log("checking bodyChanged: detected.");
+				this.onDidChangeEmitter.fire(uri);
+			}
+		}
+	}
+
+	getHeaderText() {
+		var header: string;
+		if (this.loadEnd) {
+			header = "[Load finished from: " + this.uri.path + "]";
+		} else {
+			header = "[Reading URI: " + this.uri.path + "]";
+		}
+		if (this.moreDataExist) {
+			header += "\n[WARNING: Not all data will be loaded this time.]"
+		}
+		return header;
+	}
+
+	getFooterText() {
+		var footer: string;
+		if (this.loadError != null) {
+			footer = "[Load Error: " + this.loadError + "]";
+		}
+		else {
+			if (this.loadEnd) {
+				footer = "[Load End]";
+			} else {
+				footer = "[Loading...]";
+			}
+		}
+		if (this.moreDataExist) {
+			footer += "\n[WARNING: Not all data will be loaded this time.]"
+		}
+		return footer;
+	}
+
+	async readFileStreaming(uri: vscode.Uri) {
+		try {
+			var response = await axios.get(uri.path, {
+				responseType: 'stream',
+				headers: {
+					'Authorization': 'Basic ' + this.apiKey
+				}
+			});
+			console.log("response received. ", response.data);
+			var message = response.data as IncomingMessage;
+			if (message.headers["more-data"]) {
+				this.moreDataExist = true;
+			}
+			message.on('data', (chunk: Buffer) => {
+				if (this.stopRequested) return;
+				// Output the content as a string
+				console.log("on data: " + chunk.length + "bytes");
+				this.body += chunk;
+				this.bodyChanged = true;
+			});
+			message.on('end', () => {
+				if (this.stopRequested) return;
+				// Output the content as a string
+				console.log("on end", response);
+				console.log(`complete: ${message.complete} aborted: ${message.aborted} more-data: ${message.headers["more-data"]}`);
+				this.bodyChanged = false;
+				this.loadEnd = true;
+				this.onDidChangeEmitter.fire(uri);
+			});
+		}
+		catch (error) {
+			if (this.stopRequested) return;
+			console.log(error);
+			this.loadError = error;
+			this.onDidChangeEmitter.fire(uri);
+		}
+	}
+}
+
+export class CloudBuildLogContentProvider implements vscode.TextDocumentContentProvider {
+	apiKey: string;
+	onDidChangeEmitter = new vscode.EventEmitter<vscode.Uri>();
+	onDidChange = this.onDidChangeEmitter.event;
+
+	readers: { [uri: string]: CloudBuildLogContentReader } = {};
+
+	constructor() {
+		console.log("CloudBuildLogContentProvider constructor");
+	}
+
+	setApiKey(apiKey: string) {
+		this.apiKey = apiKey;
+	}
+
+	provideTextDocumentContent(uri: vscode.Uri): string {
+		if (this.apiKey === null || this.apiKey == "") {
+			return "[Please set the apiKey setting in UnityCloudBuild-Viewer category]";
+		}
+		console.log("provideTextDocumentContent");
+		let reader = this.getReaderOrCreateFor(uri);
+		console.log("calling reader.getContent()");
+		return reader.getContent();
+	}
+
+	getReaderOrCreateFor(uri: vscode.Uri) {
+		console.log("getReaderOrCreateFor");
+		const uriString = uri.toString();
+		let reader: CloudBuildLogContentReader;
+		if (this.readers[uriString] != null) {
+			reader = this.readers[uriString];
+		}
+		else {
+			reader = new CloudBuildLogContentReader(this.apiKey, uri, this.onDidChangeEmitter);
+			this.readers[uriString] = reader;
+		}
+		return reader;
+	}
+
+	reload(uri: vscode.Uri) {
+		var uriString = uri.toString();
+		if (this.readers[uriString] != null) {
+			const reader = this.readers[uriString];
+			reader.stop();
+			this.readers[uriString] = null;
+		}
+		// just notifying change will generate fresh reader.
+		this.onDidChangeEmitter.fire(uri);
+	}
+}
+
